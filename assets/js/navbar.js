12323196c383a9f72ecfd8fa7a451f9b2edb255b
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-21 13:58:24 -0300

    Initial commit

---
+++ assets/js/navbar.js
@@ -0,0 +1,23 @@
+/* Main loop */
+function main() {
+  var nav = document.getElementById("nav");
+  var offset = nav.offsetTop + nav.offsetHeight
+
+  /* Shrink Header after scrolling by Navbar */
+  if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+    document.getElementById("logo").style.height = "1rem";
+  } else {
+    document.getElementById("logo").style.height = "2.5rem";
+  }
+
+  /* Repeat again after 500ms */
+  setTimeout(
+    function() {
+      main();
+    },
+    500
+  );
+}
+
+/* Call main function on load */
+main();
