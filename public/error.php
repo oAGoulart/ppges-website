4101b1db3554343822f95b2865b8ae37fd0690f5
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-23 02:58:52 -0300

    Fix HTTP error response

--- public/error.php
+++ public/error.php
@@ -1,25 +1,10 @@
-<?php
-  // get headers response code
-  function getResponseCode( $headers )
-  {
-    foreach( $headers as $k => $v ) {
-      $t = explode( ':', $v, 2 );
-
-      $head[] = $v;
-      if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#", $v, $out ) )
-        return intval($out[1]);
-    }
-
-    return 503;
-  }
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>error</title>
 </head>
 <body>
-  <?php echo 'Error:' , getResponseCode($http_response_header) ?>
+  <?php echo 'Error:' , http_response_code() ?>
 
 </body>
 </html>
