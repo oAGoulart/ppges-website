452ba04c8d69b57aa4988decd5eb92df8d5bd2b5
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-23 03:55:52 -0300

    Fix require_once path

--- public/sobre.php
+++ public/sobre.php
@@ -4,6 +4,7 @@
   include 'assets/templates/head.php';
   include "assets/templates/$lang/non_sticky_nav.php";
   include "assets/templates/$lang/sticky_header.php";
+  require_once '../vendor/autoload.php';
 ?>
 
   <!-- Page's Contents -->
