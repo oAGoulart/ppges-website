7c8996b5068bc8c6da2b37151f781daf48c359f0
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-22 23:35:11 -0300

    Fix stylesheet path

--- public/assets/templates/head.php
+++ public/assets/templates/head.php
@@ -28,7 +28,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Roboto:500,700&display=swap" rel="stylesheet">
 
   <!-- Default Styles CSS -->
-  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href=<?php echo get_home_url() , "/assets/css/styles.css"; ?>>
 
   <?php
     if ( isset( $page_title ) ) {
