6b55dbf57f361c44ccaf56e896650b4bc4b17271
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-24 14:50:06 -0300

    Change search page

--- public/search.php
+++ public/search.php
@@ -1,22 +1,32 @@
 <?php
-  $query = $_GET['query'];
+  if (isset($_GET['query'])) {
+    $query = $_GET['query'];
 
-  $page_title = "Buscar: $query";
+    if (isset($_GET['coll']))
+      $coll = $_GET['coll'];
+  }
+  else {
+    $query = '';
+    $coll = 'posts';
+  }
+
+  $page_title = "Buscar $query";
 
   require_once '../vendor/autoload.php';
   include 'assets/templates/head.php';
   include "assets/templates/$lang/non_sticky_nav.php";
   include "assets/templates/$lang/sticky_header.php";
 
-  echo "<p>${query}</p>";
-  $filter = ['name' => $query];
-  $options = [];
+  echo "<h1>Resultados de ${query} em ${coll}:</h1>";
+
+  $filter = ['body' => $query];
+  $options = ['allowPartialResults' => true];
 
   $q = new MongoDB\Driver\Query($filter, $options);
-  $rows = $manager->executeQuery('sample_mflix.comments', $q); 
-  foreach ($rows as $r) {
-    var_dump($r);
-  }
+  $rows = $manager->executeQuery("sample_training.$coll", $q); 
+
+  foreach ($cursor as $document)
+    var_dump($document);
 
   include "assets/templates/$lang/footer.php";
   include 'assets/templates/foot.php';
