f8319bb70a2d57be9bd10e7e9df1200e9c962ebd
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-29 03:53:38 -0300

    Change database functions

--- public/search.php
+++ public/search.php
@@ -11,16 +11,21 @@
   include "assets/templates/$lang/non_sticky_nav.php";
   include "assets/templates/$lang/sticky_header.php";
 
-  $projection = [
-    'body' => 1,
-    'permalink' => 1,
-    'author' => 1,
-    'title' => 1,
-    'tags' => 1,
-    'date' => 1
-  ];
 
-  $cursor = query_search($query, $page_size, $page_number, $projection, $database, $collection, $manager);
+  $options = [
+    'allowPartialResults' => true,
+    'limit' => $page_size,
+    'skip' => ($page_number - 1) * $page_size,
+    'projection' => [
+      'body' => 1,
+      'permalink' => 1,
+      'author' => 1,
+      'title' => 1,
+      'tags' => 1,
+      'date' => 1
+  ]];
+
+  $cursor = query_search($query, $options, $database, $collection, $manager);
 
   echo "<h1>Resultados de ${query} em ${collection}: (" + query_count($query, $database, $collection, $manager) + ")</h1>";
 
