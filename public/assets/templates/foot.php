c319491674bd9125ccb5abed147c92ef1e01393b
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-12-01 23:15:58 -0300

    Fix Firebase import

--- public/assets/templates/foot.php
+++ public/assets/templates/foot.php
@@ -3,6 +3,12 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+  <!-- Firebase App (the core Firebase SDK) -->
+  <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-app.js"></script>
+
+  <!-- Firebase Auth -->
+  <script src="https://www.gstatic.com/firebasejs/6.2.0/firebase-auth.js"></script>
+
   <!-- Navbar JavaScript -->
   <script src=<?php echo "https://${_SERVER['SERVER_NAME']}/assets/js/navbar.js"; ?>></script>
 
