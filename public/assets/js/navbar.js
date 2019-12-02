448f0da2122d44200e865910be13af0d5c70bbee
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-12-01 21:28:59 -0300

    Add admin page

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,16 +1,16 @@
-/* Main loop */
+// Main loop
 function main() {
   var nav = document.getElementById("nav");
-  var offset = nav.offsetTop + nav.offsetHeight
+  var offset = nav.offsetTop + nav.offsetHeight;
 
-  /* Shrink Header after scrolling by Navbar */
+  // Shrink Header after scrolling by Navbar
   if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
     document.getElementById("logo").style.height = "1rem";
   } else {
     document.getElementById("logo").style.height = "2.5rem";
   }
 
-  /* Repeat again after 500ms */
+  // Repeat again after 500ms
   setTimeout(
     function() {
       main();
@@ -19,5 +19,7 @@ function main() {
   );
 }
 
-/* Call main function on load */
-main();
+// Call main function on load
+window.onload = function() {
+  main();
+};
--- public/error.php
+++ public/error.php
@@ -5,10 +5,11 @@
 </head>
 <body>
   <?php
-    if ( isset( $_SERVER['REDIRECT_STATUS'] ) )
+    if ( isset( $_SERVER['REDIRECT_STATUS'] ) ) {
       echo 'Error:' , $_SERVER['REDIRECT_STATUS'];
-    else
+    } else {
       echo 'Error:' , '503';
+    }
   ?>
 </body>
 </html>
