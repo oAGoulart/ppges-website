3e93ebd9833b55c6ea0fdcd7fd2177416f34d17c
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-12-03 03:20:09 -0300

    Fix admin login verification

---
+++ public/assets/js/lang.js
@@ -0,0 +1,7 @@
+function setCookie(name, value, days) {
+  var d = new Date();
+  d.setTime(d.getTime() + (days*24*60*60*1000));
+
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = name + "=" + value + "; " + expires + "; path=/";
+}
