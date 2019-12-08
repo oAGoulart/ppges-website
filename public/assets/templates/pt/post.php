d67045381b56e0d7907c89b6cdf20bac9fa32574
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-12-08 00:55:30 -0300

    Add post location

--- public/assets/templates/pt/post.php
+++ public/assets/templates/pt/post.php
@@ -1,6 +1,30 @@
   <!-- Page's Contents -->
   <main class="my-5">
     <div class="container">
+      <div class="row">
+        <p>
+          <?php
+            $path_link = explode($_SERVER['REQUEST_URI']);
+            $next_link = '/';
+            $path_size = count($path_link);
+
+            foreach ($path_link as $i => $path) {
+              if ($i == 0) {
+                $path = 'Início';
+              }
+
+              if ($i < $path_size-1) {
+                echo '<a href="', $next_link, '">', $path, '</a>';
+                echo '<span> &#9473;&#62; </span>';
+
+                $next_link .= $path_link[$i+1] . '/';
+              } else {
+                echo '<a href="', $next_link, '">', $path, '</a>';
+              }
+            }
+          ?>
+        </p>
+      </div>
       <div class="row flex-md-row-reverse">
         <!-- Discover Links -->
         <div class="col-md-3">
