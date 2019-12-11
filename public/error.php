cb211436156b9cab6d96bbdbe8904d67eec742c5
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-12-11 14:50:25 -0300

    Fix minor issues before presentation

--- public/error.php
+++ public/error.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>error</title>
-</head>
-<body>
-  <?php
-    if (isset($_SERVER['REDIRECT_STATUS'])) {
-      echo 'Error:', $_SERVER['REDIRECT_STATUS'];
-    } else {
-      echo 'Error:', '503';
-    }
-  ?>
-</body>
-</html>
+<?php
+  require 'assets/templates/head.php';
+
+  if (isset($_SERVER['REDIRECT_STATUS'])) {
+    echo 'Error:', $_SERVER['REDIRECT_STATUS'];
+  } else {
+    echo 'Error:', '503';
+  }
+
+  require 'assets/templates/foot.php';
